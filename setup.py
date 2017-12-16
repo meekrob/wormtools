1bad439a350a1c76fc887d0249e2c275935c68a0
David King <davidcking.inbox@gmail.com>
2017-12-15 17:24:16 -0700

    More metadata, README

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ setup(
     name="wormtools",
     version="0.1",
     packages=find_packages(),
-    scripts=["search_seq_motif.py"]
+    scripts=["search_seq_motif.py"],
+    author="David King",
+    author_email="davidcking.inbox@gmail.com",
+    description="Basic and advanced tools for bioinformatics on c. elegans.",
+    keywords="python bioinformatics c.elegans",
+    url="https://github.com/meekrob/wormtools"
 )
