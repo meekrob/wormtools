050b6e648893378a4d2330cd8117ada4574d5157
David King <davidcking.inbox@gmail.com>
2017-12-16 11:20:49 -0700

    Split out GFF/GFFParser.py

--- src/wormtools/GFF/__init__.py
+++ src/wormtools/GFF/__init__.py
@@ -16,22 +16,27 @@ def GFF_as_BED6(gff):
 def gene_id(gff): # gff is normal dict
     """
     Convenience method to get the scalar gene_id out of a GFF returned by GFF.GFFParser
+    >>> gff = {'attr':{'gene_id':['sample_gene_id']}}
+    >>> gene_id(gff)
+    'sample_gene_id'
     """
     return get_scalar_attr(gff, 'gene_id')
-    """
-    >>> GFF.gene_id({'attr':{'gene_id':['sample_gene_id']})
-    ['ample_gene_id']
-    """
     
 def get_scalar_attr(gff, attr_name): # gff is normal dict
     """
     Convenience method to get a scalar attribute out of a GFF returned by GFF.GFFParser
+    >>> gff = {'attr':{'sample_scalar_attr_name':['sample_scalar_attr_value']}}
+    >>> get_scalar_attr(gff, 'sample_scalar_attr_name')
+    'sample_scalar_attr_value'
     """
     return get_attr(gff, attr_name)[0]
 
 def get_attr(gff, attr_name):
     """
     Convenience method to return an attribute list from a GFF returned by GFF.GFFParser, or parse the 'attr_str' if it hasn't happened yet.
+    >>> gff = {'attr':{'sample_attr_name':['sample_attr_value']}}
+    >>> get_attr(gff, 'sample_attr_name')
+    ['sample_attr_value']
     """
     if not gff.has_key('attr'):
         if gff.has_key('attr_str'):
